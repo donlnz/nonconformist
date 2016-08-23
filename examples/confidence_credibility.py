28a172eb26a9113cfc846ca45c358dade3411e7a
Henrik Linusson <henrik.linusson@gmail.com>
2016-08-23 13:31:01 +0200

    Added a small example using confidence-and-credibility predictions.

---
+++ examples/confidence_credibility.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+from nonconformist.icp import IcpClassifier
+from nonconformist.nc import ProbEstClassifierNc
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+
+data = load_iris()
+x, y = data.data, data.target
+
+for i, y_ in enumerate(np.unique(y)):
+	y[y == y_] = i
+
+n_instances = y.size
+idx = np.random.permutation(n_instances)
+
+train_idx = idx[:int(n_instances / 3)]
+cal_idx = idx[int(n_instances / 3):2 * int(n_instances / 3)]
+test_idx = idx[2 * int(n_instances / 3):]
+
+nc = ProbEstClassifierNc(RandomForestClassifier())
+icp = IcpClassifier(nc)
+
+icp.fit(x[train_idx, :], y[train_idx])
+icp.calibrate(x[cal_idx, :], y[cal_idx])
+
+
+print(pd.DataFrame(icp.predict_conf(x[test_idx, :]),
+				   columns=['Label', 'Confidence', 'Credibility']))
