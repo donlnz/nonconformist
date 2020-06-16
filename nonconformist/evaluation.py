c54113e9cdd1051e4393e2ba2e00840d8c8aa21c
Henrik Linusson <henrik@ekkono.ai>
2020-06-16 12:06:31 +0200

    Updated to work with current scikit-learn API for model selection

--- nonconformist/evaluation.py
+++ nonconformist/evaluation.py
@@ -16,9 +16,9 @@ import sys
 import numpy as np
 import pandas as pd
 
-from sklearn.cross_validation import StratifiedShuffleSplit
-from sklearn.cross_validation import KFold
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 from sklearn.base import clone, BaseEstimator
 
 
